9b68cd2e78fb7122d2a7c141479af631e4c80673
anasrasyid <anasrasyid333@gmail.com>
2019-05-01 16:44:38 +0700

    level 01

---
+++ Internship/Assets/Script/Players.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Players : MonoBehaviour
+{
+    public float speed = 10f;
+    private Rigidbody2D rigidbody2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2 = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //float moveHorizontal = Input.GetAxis("Horizontal");
+        //float moveVertical = Input.GetAxis("Vertical");
+        float moveHorizontal = Input.acceleration.x;
+        float moveVertical = Input.acceleration.y;
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rigidbody2.velocity = movement * speed;
+
+    }
+}
